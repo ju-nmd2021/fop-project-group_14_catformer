13010ecd987c334fdac7e9cbe6b5f6f7736c8f12
Adina Ståhl <stad21wg@student.ju.se>
2022-05-02 16:58:59 +0200

    updated controls
    the cat can now run left and right

--- logic.js
+++ logic.js
@@ -1,46 +1,47 @@
 //here's our one source of truth for the canvas width and height
 const sWidth = 800;
 const sHeight = 600;
+
 function setup() {
-  createCanvas(sWidth, sHeight);
+    createCanvas(sWidth, sHeight);
 }
 
 //here, we wait until the document has loaded fully before setting the gamestate to start
 window.addEventListener("load", () => {
-  gameState = "start";
-  mainTitleElement = document.getElementById("mainTitle");
-  mainTitleElement.innerText = "Not loading anymore";
+    gameState = "start";
+    mainTitleElement = document.getElementById("mainTitle");
+    mainTitleElement.innerText = "Not loading anymore";
 });
 
 //sprite for our main cat :3
 function catSprite(obj) {
-  push();
-  strokeWeight(0);
-  fill(0, 0, 0);
-  rect(obj.x - 20, obj.y - 25, 40, 20); //body
-  rect(obj.x - 20, obj.y - 6, 5, 5); //leg 1
-  rect(obj.x - 10, obj.y - 6, 5, 5); //leg 2
-  rect(obj.x + 5, obj.y - 6, 5, 5); //leg 3
-  rect(obj.x + 15, obj.y - 6, 5, 5); //leg 4
-  rect(obj.x + 7, obj.y - 30, 20, 20); //head
-  rect(obj.x - 20, obj.y - 35, 5, 11); //tail
-  pop();
+    push();
+    strokeWeight(0);
+    fill(0, 0, 0);
+    rect(obj.x - 20, obj.y - 25, 40, 20); //body
+    rect(obj.x - 20, obj.y - 6, 5, 5); //leg 1
+    rect(obj.x - 10, obj.y - 6, 5, 5); //leg 2
+    rect(obj.x + 5, obj.y - 6, 5, 5); //leg 3
+    rect(obj.x + 15, obj.y - 6, 5, 5); //leg 4
+    rect(obj.x + 7, obj.y - 30, 20, 20); //head
+    rect(obj.x - 20, obj.y - 35, 5, 11); //tail
+    pop();
 }
 
 //sprite for the floor
 function CollisionBlockSprite(obj) {
-  strokeWeight(0);
-  if (obj.type === "floor") {
-    push();
-    fill(150, 80, 50);
-    rect(obj.x, sHeight - obj.height, obj.width, obj.height);
-    pop();
-  } else if (obj.type === "shelf") {
-    push();
-    fill(200, 120, 50);
-    rect(obj.x, sHeight - obj.height, obj.width, obj.height - floor.height);
-    pop();
-  }
+    strokeWeight(0);
+    if (obj.type === "floor") {
+        push();
+        fill(150, 80, 50);
+        rect(obj.x, sHeight - obj.height, obj.width, obj.height);
+        pop();
+    } else if (obj.type === "shelf") {
+        push();
+        fill(200, 120, 50);
+        rect(obj.x, sHeight - obj.height, obj.width, obj.height - floor.height);
+        pop();
+    }
 }
 
 //here's all the variables we set before starting everything
@@ -50,89 +51,104 @@ let gameState = "loading";
 const gravity = 0.5;
 
 const cat = {
-  x: sWidth / 2,
-  y: sHeight - 300,
-  acceleration: 5,
-  jumpHeight: 10,
-  downSpeed: 0,
-  sideSpeed: 0,
-  state: "stand",
+    x: sWidth / 2,
+    y: sHeight - 300,
+    acceleration: 5,
+    jumpHeight: 10,
+    downSpeed: 0,
+    sideSpeed: 0,
+    state: "stand",
 };
 
 const floor = {
-  x: 0,
-  width: sWidth,
-  height: 50,
-  type: "floor",
-  dangerous: false,
+    x: 0,
+    width: sWidth,
+    height: 50,
+    type: "floor",
+    dangerous: false,
 };
 
 const shelf1 = {
-  x: 500,
-  width: 100,
-  height: 100,
-  type: "shelf",
-  dangerous: false,
+    x: 500,
+    width: 100,
+    height: 100,
+    type: "shelf",
+    dangerous: false,
 };
 
 const collisionBlocks = [floor, shelf1];
 
 //The main draw function that is called many times per second
 function draw() {
-  if (gameState === "loading") {
-    //here we could have a loading screen if we want it, but it might be unnecessary
-  } else if (gameState === "start") {
-    //Here's where we summon the start screen
-    background(250, 230, 150);
-    text("press R to start playing", 300, 300);
-  } else if (gameState === "play") {
-    //here's where we have all the gameplay code
-    background(250, 230, 150);
+    if (gameState === "loading") {
+        //here we could have a loading screen if we want it, but it might be unnecessary
+    } else if (gameState === "start") {
+        //Here's where we summon the start screen
+        background(250, 230, 150);
+        text("press R to start playing", 300, 300);
+    } else if (gameState === "play") {
+        //here's where we have all the gameplay code
+        background(250, 230, 150);
 
-    for (let block of collisionBlocks) {
-      CollisionBlockSprite(block);
+        for (let block of collisionBlocks) {
+            CollisionBlockSprite(block);
+        }
+
+        catSprite(cat);
+
+        //controls the gravity and falling
+        let collisionDetection = 0;
+        for (let block of collisionBlocks) {
+            if (
+                cat.y >= sHeight - block.height &&
+                cat.x < block.x + block.width &&
+                cat.x > block.x
+            ) {
+                collisionDetection++;
+            }
+        }
+        if (collisionDetection > 0 && cat.downSpeed >= 0) {
+            cat.state = "stand";
+            cat.downSpeed = 0;
+        } else {
+            cat.state = "fall";
+            cat.downSpeed += gravity;
+        }
+
+        cat.y += cat.downSpeed;
+
+        // controls of moving left and right (looked at garrits lecture "12: Example - Move a cat with the keyboard" and took inspiration for this section)
+        cat.x = cat.x + cat.sideSpeed;
+        // left arrow:
+        if (keyIsDown(37)) {
+            cat.sideSpeed = -5;
+        }
+        // right arrow:
+        else if (keyIsDown(39)) {
+            cat.sideSpeed = 5;
+        } else {
+            cat.sideSpeed = 0;
+        }
     }
-
-    catSprite(cat);
-
-    //controls the gravity and falling
-    let collisionDetection = 0;
-    for (let block of collisionBlocks) {
-      if (
-        cat.y >= sHeight - block.height &&
-        cat.x < block.x + block.width &&
-        cat.x > block.x
-      ) {
-        collisionDetection++;
-      }
-    }
-    if (collisionDetection > 0 && cat.downSpeed >= 0) {
-      cat.state = "stand";
-      cat.downSpeed = 0;
-    } else {
-      cat.state = "fall";
-      cat.downSpeed += gravity;
-    }
-
-    cat.y += cat.downSpeed;
-  }
 }
 
 function keyPressed() {
-  console.log(keyCode);
-  if (gameState === "start" && keyCode === 82) {
-    gameState = "play";
-  }
-  if (keyCode === 32 && cat.state === "stand") {
-    cat.downSpeed = cat.jumpHeight * -1;
-  }
-  // left arrow
-  if (keyCode === 37 && cat.state === "stand") {
-    cat.x = cat.x - 10;
-  }
+    console.log(keyCode);
+    if (gameState === "start" && keyCode === 82) {
+        gameState = "play";
+    }
+    if (keyCode === 32 && cat.state === "stand") {
+        cat.downSpeed = cat.jumpHeight * -1;
+    }
+    /*     // left arrow
+        if (keyCode === 37) {
+            // cat.x = cat.x - 10;
+            cat.x = cat.x * -1;
 
-  // right arrow
-  if (keyCode === 39 && cat.state === "stand") {
-    cat.x = cat.x + 10;
-  }
+        }
+
+        // right arrow
+        if (keyCode === 39) {
+            cat.x = cat.x + 10;
+        } */
 }
