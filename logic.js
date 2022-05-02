31cc61fa3ebc1e32b400b7c52abe35f7b7729c21
Kimpes <gustafssonfilip@hotmail.com>
2022-05-02 09:46:02 +0200

    Added a canvas with the p5

--- logic.js
+++ logic.js
@@ -1,6 +1,10 @@
 let gameState = "loading"; //first we set the gamestate to loading
 let mainTitleElement;
 
+function setup() {
+  createCanvas(800, 600);
+}
+
 window.addEventListener("load", () => {
   gameState = "start"; //when the entire document is done loading, we draw the start screen
   mainTitleElement = document.getElementById("mainTitle");
@@ -11,6 +15,7 @@ function draw() {
   if (gameState === "loading") {
     //here we could have a loading screen if we want it, but it might be unnecessary
   } else if (gameState === "start") {
+    background(250, 230, 150);
     //Here's where we summon the start screen
   } else if (gameState === "play") {
     //here's where we have all the gameplay code
