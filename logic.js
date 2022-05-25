2e6fcee23300f685e22327a781d00c2bbba1dddf
Kimpes <gustafssonfilip@hotmail.com>
2022-05-25 15:21:53 +0200

    Reorganised code into functions
    Instead of writing all the code in the draw function, we now have several functions that we call from the draw function

--- logic.js
+++ logic.js
@@ -197,7 +197,7 @@ let playerScore;
 let playerPosition;
 
 const cat = {
-  x: sWidth / 2,
+  x: 100,
   y: sHeight - 30,
   acceleration: 0.5,
   jumpHeight: 17,
@@ -299,14 +299,14 @@ const shelf5 = {
 };
 
 const vacuum1 = {
-  x: 100,
+  x: 300,
   y: sHeight - 80,
   width: 80,
   height: 30,
   type: "vacuum",
   dangerous: true,
-  startPoint: 50,
-  endPoint: 250,
+  startPoint: 250,
+  endPoint: 500,
   speed: 2,
 };
 
@@ -395,11 +395,11 @@ const collisionBlocks = [
   shelf5, // the shelf that holds the vase
   wallLeft,
   wallRight,
-  vase,
   fireplaceSurface,
   windowSurfaceBottom,
   windowSurfaceTop,
   stoolSurface,
+  vase,
 ];
 //list of all NON collision blocks
 const nonCollisionBlocks = [fireplace1, window1, stool];
@@ -442,248 +442,47 @@ function draw() {
   } else if (gameState === "play") {
     //here's where we have all the gameplay code
 
-    // drawing the room:
     background(wallColor); // wallpaper
-    //image(fireplaceImg, sWidth - 300 - (338 / 2), sHeight / 2, 338, 301); // fireplace
 
     // Resetting the vase img
     vase.broken = false;
 
-    // Testing countdown timer based on this tutorial: https://www.youtube.com/watch?v=rKhwDhp9dcs&ab_channel=flanniganable
+    timeLogic();
 
-    currentTimeSec = int(millis() / 1000); // this runs in the background and counts how many seconds has passed
-    currentTimeMil = int(millis() / 10); // this runs in the background and counts how many milliseconds has passed
+    //renders all platforms and obstacles
+    renderAllSprites();
 
-    countDown = timeLimit - (currentTimeSec - startTimeSec) - timePenalty;
-    let countDownMil = timeLimit - (currentTimeMil - startTimeMil);
-
-    //if counter reaches 0, you've run out of time
-    if (countDown < 0) {
-      countDown = 0;
-      gameState = "loose";
-    }
-
-    //every 5 milliseconds, the cat goes into the second run sprite
-    if ((currentTimeMil / 5) % 2 === 0) {
-      cat.runSprite *= -1;
-    }
-
-    if (frameCounter === 60) {
-      frameCounter = 0;
-    }
-    frameCounter++;
-
-    //every 10th frame, the run sprite is activated
-    if (frameCounter % 10 === 0) {
-      cat.runSprite *= -1;
-    }
-
-    // creating a timer
-    push();
-    //let timer = "10:00:00";
-    fill(0, 0, 0);
-    textAlign("center");
-    textSize(36);
-    text(countDown + ":" + (countDownMil % 100), sWidth - 150, 50);
-    pop();
-
-    //draws all collision blocks
-    for (let block of collisionBlocks) {
-      CollisionBlockSprite(block);
-    }
-
-    //draws all NON collision blocks
-    for (let block of nonCollisionBlocks) {
-      CollisionBlockSprite(block);
-    }
-
-    //draws all the obstacles
-    for (let obstacle of obstacles) {
-      CollisionBlockSprite(obstacle);
-    }
-
+    //draws the cat character
     catSprite(cat);
 
     // Moving the cat
-    // left arrow:
-    if (keyIsDown(37) || keyIsDown(65)) {
-      if (cat.direction === "right") {
-        cat.direction = "left";
-      }
-      // with acceleration
-      // cat.sideSpeed -= cat.acceleration;
-
-      //without acceleration
-      cat.sideSpeed -= cat.acceleration;
-      if (cat.sideSpeed < -cat.maxSpeed) {
-        cat.sideSpeed = -cat.maxSpeed;
-      }
-    } // right arrow:
-    else if (keyIsDown(39) || keyIsDown(68)) {
-      // right arrow:
-      if (cat.direction === "left") {
-        cat.direction = "right";
-      }
-      // with acceleration
-      // cat.sideSpeed += cat.acceleration;
-
-      //without acceleration
-      cat.sideSpeed += cat.acceleration;
-      if (cat.sideSpeed > cat.maxSpeed) {
-        cat.sideSpeed = cat.maxSpeed;
-      }
-    } // cat's not moving:
-    else {
-      cat.sideSpeed *= 0.8;
-      if (cat.sideSpeed < 0.01 && cat.sideSpeed > -0.01) {
-        cat.sideSpeed = 0;
-      }
-    }
+    catSideMovement();
 
     //controls collision
-    cat.downSpeed += gravity;
-    let hasCollided = false;
-    for (let block of collisionBlocks) {
-      //checks if the cat is within the bounds of a box. has the cat collided with anything?
-      if (
-        cat.x - 10 < block.x + block.width &&
-        cat.x + 10 > block.x &&
-        cat.y - 5 < block.y + block.height &&
-        cat.y + 5 > block.y
-      ) {
-        hasCollided = true;
-        //if yes, let's check from what direction the cat must have come from
-        let distanceToTop = cat.y - block.y;
-        let distanceToBottom =
-          sHeight - cat.y - (sHeight - (block.y + block.height));
-        let distanceToLeft = cat.x - block.x;
-        let distanceToRight =
-          sWidth - cat.x - (sWidth - (block.x + block.width));
-
-        //the smallest direction to outside the block must be from what direction the cat collided. let's find the shortest distance
-        let distanceCompareArray = [
-          distanceToTop,
-          distanceToBottom,
-          distanceToLeft,
-          distanceToRight,
-        ];
-
-        //syntax below sourced from https://stackoverflow.com/questions/8934877/obtain-smallest-value-from-array-in-javascript
-        let smallestDistance = Math.min(...distanceCompareArray);
-        let indexOfSmallestDistance =
-          distanceCompareArray.indexOf(smallestDistance);
-
-        if (indexOfSmallestDistance === 0) {
-          //smallest distance is to the top of the block. the cat has landed
-          if (cat.downSpeed > 0) {
-            cat.downSpeed = 0;
-            cat.y -= smallestDistance - 1;
-          }
-          cat.state = "stand";
-        } else if (indexOfSmallestDistance === 1) {
-          //smallest distance is to the bottom of the block. the cat has bumped a ceiling
-          if (cat.downSpeed < 0) {
-            cat.downSpeed = 0;
-          }
-          cat.state = "fall";
-        } else if (indexOfSmallestDistance === 2) {
-          //smallest distance is to the left side of a wall. the cat has bumped into a wall on the right
-          if (cat.sideSpeed > 0) {
-            cat.sideSpeed = 0;
-          }
-        } else if (indexOfSmallestDistance === 3) {
-          //smallest distance is to the right side of a wall. the cat has bumped into a wall on the left
-          if (cat.sideSpeed < 0) {
-            cat.sideSpeed = 0;
-          }
-        }
-
-        // if statement for if the cat has collided with the vase
-        if (block.type === "vase") {
-          playerScore = countDown;
-          playerPosition = checkScore(playerScore);
-          vase.speed = -10;
-          gameState = "win";
-        }
-      } else {
-        //if the cat hasn't collided with anything, it will be affected by gravity
-      }
-    }
-
-    //if the cat didn't collide with anything, it should be in the state "fall"
-    if (hasCollided === false) {
-      cat.state = "fall";
-    }
+    catDetectCollision();
 
     //collision with things that hurt you
-    for (let obstacle of obstacles) {
-      if (
-        cat.x - 10 < obstacle.x + obstacle.width &&
-        cat.x + 10 > obstacle.x &&
-        cat.y - 5 < obstacle.y + obstacle.height &&
-        cat.y + 5 > obstacle.y &&
-        cat.invincibility === 0
-      ) {
-        //makes the cat jump upwards when taking damage
-        cat.downSpeed = -10;
-
-        //this makes it so that the cat doesn't get hurt many times in a row
-        cat.invincibility = 60;
-
-        //this reduces the timer by 1 as your penalty
-        timePenalty++;
-      }
-    }
-
-    //this counts down the invincibility
-    if (cat.invincibility > 0) {
-      cat.invincibility--;
-    }
+    catDetectDamage();
 
     cat.y += cat.downSpeed;
-
     // controls of moving left and right (looked at garrits lecture "12: Example - Move a car with the keyboard" and took inspiration for this section)
     cat.x = cat.x + cat.sideSpeed;
 
-    // What does this one to?
-    for (obstacle of obstacles) {
-      obstacle.x += obstacle.speed;
-      if (obstacle.x > obstacle.endPoint || obstacle.x < obstacle.startPoint) {
-        obstacle.speed *= -1;
-      }
-    }
+    // moves the obstacles that move
+    obstacleMovement();
+
     ///// End of play state
   } else if (gameState === "win") {
     // Here's the screen if you win the game
     background(wallColor);
-
-    for (let block of nonCollisionBlocks) {
-      CollisionBlockSprite(block);
-    }
-
-    for (let obstacle of obstacles) {
-      CollisionBlockSprite(obstacle);
-    }
-
-    for (let block of collisionBlocks) {
-      CollisionBlockSprite(block);
-    }
+    renderAllSprites();
 
     // resetting cat position
-    cat.x = sWidth / 2;
+    cat.x = 100;
     cat.y = sHeight - 30;
 
-    if (vase.y < sHeight - floor.height) {
-      vase.speed += gravity;
-      vase.x += 5;
-      vase.y += vase.speed;
-      vase.rotation += 0.1;
-    } else {
-      vase.rotation = 0;
-      vase.broken = true;
-      console.log("rotation: " + vase.rotation);
-      console.log("vase x: " + vase.x);
-    }
+    //throws the vase off of the shelf
+    vaseMovement();
 
     textAlign(CENTER);
 
@@ -698,6 +497,8 @@ function draw() {
     push();
     gameText = "I AM THE SUPERIOR CAT! SUCK IT, GRAVITY!";
     text(gameText, sWidth / 2, sHeight / 5);
+
+    //score text
     push();
     textSize(20);
     text(
@@ -706,36 +507,11 @@ function draw() {
       sHeight / 4
     );
 
-    //if you ended up 6th on the scoreboard, you don't get to join
-    if (playerPosition === 5) {
-      text("You didn't get on the leaderboard", sWidth / 2, sHeight / 3);
-    } else if (playerPosition === -1) {
-      //if you didn't write your name, you don't get to join
-      text("You didn't enter your name in the field!", sWidth / 2, sHeight / 3);
-    } else {
-      //otherwise, you've joined the scoreboard
-      let positionOnLeaderboard = playerPosition + 1;
-      text(
-        "You got position " + positionOnLeaderboard + " on the leaderboard!",
-        sWidth / 2,
-        sHeight / 3
-      );
-    }
+    //Tells the player what results they got and if they got on the scoreboard
+    displayPlayerResult();
 
     //Loops through all results from the scoreboard and displays them
-    push();
-    textSize(20);
-    let userPosition = 0;
-    for (let index in sessionScoreBoard) {
-      let player = sessionScoreBoard[index];
-      userPosition++;
-      text(
-        userPosition + ". " + player.name + ": " + player.score + "s",
-        sWidth / 2,
-        sHeight / 2 + index * 20
-      );
-    }
-    pop();
+    displayScoreboard();
 
     pop();
     text("press Enter to play again", sWidth / 2, sHeight - 100);
@@ -772,6 +548,229 @@ function draw() {
   }
 }
 
+//keeps track of the timer and draws it on the canvas
+function timeLogic() {
+  // Testing countdown timer based on this tutorial: https://www.youtube.com/watch?v=rKhwDhp9dcs&ab_channel=flanniganable
+  currentTimeSec = int(millis() / 1000); // this runs in the background and counts how many seconds has passed
+  currentTimeMil = int(millis() / 10); // this runs in the background and counts how many milliseconds has passed
+
+  countDown = timeLimit - (currentTimeSec - startTimeSec) - timePenalty;
+  let countDownMil = timeLimit - (currentTimeMil - startTimeMil);
+
+  //if counter reaches 0, you've run out of time
+  if (countDown < 0) {
+    countDown = 0;
+    gameState = "loose";
+  }
+
+  //every 5 milliseconds, the cat goes into the second run sprite
+  if ((currentTimeMil / 5) % 2 === 0) {
+    cat.runSprite *= -1;
+  }
+
+  if (frameCounter === 60) {
+    frameCounter = 0;
+  }
+  frameCounter++;
+
+  //every 10th frame, the run sprite is activated
+  if (frameCounter % 10 === 0) {
+    cat.runSprite *= -1;
+  }
+
+  // creating a timer
+  push();
+  //let timer = "10:00:00";
+  fill(0, 0, 0);
+  textAlign("center");
+  textSize(36);
+  text(countDown + ":" + (countDownMil % 100), sWidth - 150, 50);
+  pop();
+}
+
+//renders all blocks that the cat can impact, both visible and invisible, dangerous and not
+function renderAllSprites() {
+  //draws all NON collision blocks
+  for (let block of nonCollisionBlocks) {
+    CollisionBlockSprite(block);
+  }
+
+  //draws all collision blocks
+  for (let block of collisionBlocks) {
+    CollisionBlockSprite(block);
+  }
+
+  //draws all the obstacles
+  for (let obstacle of obstacles) {
+    CollisionBlockSprite(obstacle);
+  }
+}
+
+//the logic for the cat's sideways movement
+function catSideMovement() {
+  if (keyIsDown(37) || keyIsDown(65)) {
+    if (cat.direction === "right") {
+      cat.direction = "left";
+    }
+    // with acceleration
+    // cat.sideSpeed -= cat.acceleration;
+
+    //without acceleration
+    cat.sideSpeed -= cat.acceleration;
+    if (cat.sideSpeed < -cat.maxSpeed) {
+      cat.sideSpeed = -cat.maxSpeed;
+    }
+  } // right arrow:
+  else if (keyIsDown(39) || keyIsDown(68)) {
+    // right arrow:
+    if (cat.direction === "left") {
+      cat.direction = "right";
+    }
+    // with acceleration
+    // cat.sideSpeed += cat.acceleration;
+
+    //without acceleration
+    cat.sideSpeed += cat.acceleration;
+    if (cat.sideSpeed > cat.maxSpeed) {
+      cat.sideSpeed = cat.maxSpeed;
+    }
+  } // cat's not moving:
+  else {
+    cat.sideSpeed *= 0.8;
+    if (cat.sideSpeed < 0.01 && cat.sideSpeed > -0.01) {
+      cat.sideSpeed = 0;
+    }
+  }
+}
+
+//some obstacles such as the vacuum cleaner move around. here's that logic
+function obstacleMovement() {
+  for (obstacle of obstacles) {
+    obstacle.x += obstacle.speed;
+    if (obstacle.x > obstacle.endPoint || obstacle.x < obstacle.startPoint) {
+      obstacle.speed *= -1;
+    }
+  }
+}
+
+//upon winning, throws the vase off the shelf
+function vaseMovement() {
+  if (vase.y < sHeight - floor.height) {
+    vase.speed += gravity;
+    vase.x += 5;
+    vase.y += vase.speed;
+    vase.rotation += 0.1;
+  } else {
+    vase.rotation = 0;
+    vase.broken = true;
+    console.log("rotation: " + vase.rotation);
+    console.log("vase x: " + vase.x);
+  }
+}
+
+//checks if the cat has impacted a surface
+function catDetectCollision() {
+  cat.downSpeed += gravity;
+  let hasCollided = false;
+  for (let block of collisionBlocks) {
+    //checks if the cat is within the bounds of a box. has the cat collided with anything?
+    if (
+      cat.x - 10 < block.x + block.width &&
+      cat.x + 10 > block.x &&
+      cat.y - 5 < block.y + block.height &&
+      cat.y + 5 > block.y
+    ) {
+      hasCollided = true;
+      //if yes, let's check from what direction the cat must have come from
+      let distanceToTop = cat.y - block.y;
+      let distanceToBottom =
+        sHeight - cat.y - (sHeight - (block.y + block.height));
+      let distanceToLeft = cat.x - block.x;
+      let distanceToRight = sWidth - cat.x - (sWidth - (block.x + block.width));
+
+      //the smallest direction to outside the block must be from what direction the cat collided. let's find the shortest distance
+      let distanceCompareArray = [
+        distanceToTop,
+        distanceToBottom,
+        distanceToLeft,
+        distanceToRight,
+      ];
+
+      //syntax below sourced from https://stackoverflow.com/questions/8934877/obtain-smallest-value-from-array-in-javascript
+      let smallestDistance = Math.min(...distanceCompareArray);
+      let indexOfSmallestDistance =
+        distanceCompareArray.indexOf(smallestDistance);
+
+      if (indexOfSmallestDistance === 0) {
+        //smallest distance is to the top of the block. the cat has landed
+        if (cat.downSpeed > 0) {
+          cat.downSpeed = 0;
+          cat.y -= smallestDistance - 1;
+        }
+        cat.state = "stand";
+      } else if (indexOfSmallestDistance === 1) {
+        //smallest distance is to the bottom of the block. the cat has bumped a ceiling
+        if (cat.downSpeed < 0) {
+          cat.downSpeed = 0;
+        }
+        cat.state = "fall";
+      } else if (indexOfSmallestDistance === 2) {
+        //smallest distance is to the left side of a wall. the cat has bumped into a wall on the right
+        if (cat.sideSpeed > 0) {
+          cat.sideSpeed = 0;
+        }
+      } else if (indexOfSmallestDistance === 3) {
+        //smallest distance is to the right side of a wall. the cat has bumped into a wall on the left
+        if (cat.sideSpeed < 0) {
+          cat.sideSpeed = 0;
+        }
+      }
+
+      // if statement for if the cat has collided with the vase
+      if (block.type === "vase") {
+        playerScore = countDown;
+        playerPosition = checkScore(playerScore);
+        vase.speed = -10;
+        gameState = "win";
+      }
+    } else {
+      //if the cat hasn't collided with anything, it will be affected by gravity
+    }
+  }
+
+  //if the cat didn't collide with anything, it should be in the state "fall"
+  if (hasCollided === false) {
+    cat.state = "fall";
+  }
+}
+
+//checks if the cat has taken damage and does the damaging
+function catDetectDamage() {
+  for (let obstacle of obstacles) {
+    if (
+      cat.x - 10 < obstacle.x + obstacle.width &&
+      cat.x + 10 > obstacle.x &&
+      cat.y - 5 < obstacle.y + obstacle.height &&
+      cat.y + 5 > obstacle.y &&
+      cat.invincibility === 0
+    ) {
+      //makes the cat jump upwards when taking damage
+      cat.downSpeed = -10;
+
+      //this makes it so that the cat doesn't get hurt many times in a row
+      cat.invincibility = 60;
+
+      //this reduces the timer by 1 as your penalty
+      timePenalty++;
+    }
+  }
+
+  //this counts down the invincibility
+  if (cat.invincibility > 0) {
+    cat.invincibility--;
+  }
+}
+
 //checks whether the new score is good enough to be placed on the scoreboard
 function checkScore(newScore) {
   let scoreCheck = 5;
@@ -797,11 +796,43 @@ function checkScore(newScore) {
   return scoreCheck;
 }
 
+function displayPlayerResult() {
+  if (playerPosition === 5) {
+    text("You didn't get on the leaderboard", sWidth / 2, sHeight / 3);
+  } else if (playerPosition === -1) {
+    //if you didn't write your name, you don't get to join
+    text("You didn't enter your name in the field!", sWidth / 2, sHeight / 3);
+  } else {
+    //otherwise, you've joined the scoreboard
+    let positionOnLeaderboard = playerPosition + 1;
+    text(
+      "You got position " + positionOnLeaderboard + " on the leaderboard!",
+      sWidth / 2,
+      sHeight / 3
+    );
+  }
+}
+
+function displayScoreboard() {
+  push();
+  textSize(20);
+  let userPosition = 0;
+  for (let index in sessionScoreBoard) {
+    let player = sessionScoreBoard[index];
+    userPosition++;
+    text(
+      userPosition + ". " + player.name + ": " + player.score + "s",
+      sWidth / 2,
+      sHeight / 2 + index * 20
+    );
+  }
+  pop();
+}
+
 function updateLocalStorage() {
   localStorage.scoreBoard = JSON.stringify(sessionScoreBoard);
 }
 
-// is says this isn't being used?
 function keyPressed() {
   console.log(keyCode);
   //if you press enter, you move between game states
